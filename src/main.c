2b17bd1cccfcb26adf8a76e0a379016d65f4bb80
Simon Otter <skurmedel@gmail.com>
2013-04-14 06:46:59 +0200

    File support, rudimentary though.

--- src/main.c
+++ src/main.c
@@ -5,10 +5,21 @@
 
 int main(int argc, char *argv[])
 {
-	tokeniser *t = tokeniser_setup(stdin);
+	FILE *f = stdin;
+	if (argc > 1)
+	{
+		f = fopen(argv[1], "r");
+		if (!f)
+		{
+			printf("Unknown file.\n");
+			return 1;
+		}
+	}
+
+	tokeniser *t = tokeniser_setup(f);
 
 	int c = 0;
-	while (c++ < 5)
+	while (1)
 	{
 		token tok;
 		int error = tokeniser_next(t, &tok);
@@ -20,6 +31,9 @@ int main(int argc, char *argv[])
 		}
 
 		printf("%s\n", token_name(tok));
+
+		if (tok == token_eof)
+			return 0;
 	}	
 
 	return 0;
